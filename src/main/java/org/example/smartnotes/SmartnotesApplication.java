cd5dc3f29479e9d0f0785e3a6e8204a0c61a9549
cybertobbe <tobias.sjostrom@iths.se>
2025-11-29 16:44:38 +0100

    initial commit

---
+++ src/main/java/org/example/smartnotes/SmartnotesApplication.java
@@ -0,0 +1,13 @@
+package org.example.smartnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartnotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartnotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/smartnotes/SmartnotesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.smartnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartnotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
